145abb25b06afa9def1ff4cb5b170e22a64f42fb
LuisTakeo <luistakeo@hotmail.com>
2023-04-03 01:40:10 -0300

    Requisições JS, alterado alguns trechos para desestructuring, everificado mais sobre os retornos json

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,18 +2,13 @@
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
+    console.log(pokeDetail);
+    const { id: number, name } = pokeDetail;
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
+    const photo = pokeDetail.sprites.other.dream_world.front_default
 
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
+    const pokemon = new Pokemon(number, name, types, type, photo)
     return pokemon
 }
 
@@ -27,9 +22,13 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then(response => response.json())
+        .then(jsonBody => {
+            console.log(jsonBody)
+            return jsonBody.results})
+        .then(pokemons => {
+            console.log(pokemons)
+            return pokemons.map(pokeApi.getPokemonDetail)})
+        .then(detailRequests => Promise.all(detailRequests))
+        .then(pokemonsDetails => pokemonsDetails)
 }
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,4 +5,13 @@ class Pokemon {
     type;
     types = [];
     photo;
+
+    constructor(number, name, types, type, photo){
+        this.number = number;
+        this.name = name;
+        this.types = types;
+        this.type = type;
+        this.photo = photo;
+        
+    }
 }
